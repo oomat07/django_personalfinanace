11718e9a93f36c3da3f894878aec689c223d30fb
oomat07 <shakiralievomat3@gmail.com>
2024-03-04 18:01:15 +0600

    created income detail_view and registered

--- apps/Incomes/forms.py
+++ apps/Incomes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Income
 
-class IncomeForm(forms.Modelform):
-    class Meya:
+class IncomeForm(forms.ModelForm):
+    class Meta:
         model = Income
         fields = ['source', 'amount', 'date', 'description']
--- apps/Incomes/views.py
+++ apps/Incomes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Income
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from .forms import IncomeForm
 from django.urls import reverse_lazy
 
@@ -14,3 +15,8 @@ class IncomeCreateView(CreateView):
     template_name = 'create_income.html'
     form_class = IncomeForm
     success_url = reverse_lazy('incomes_list')
+
+class IncomeDetailView(DetailView):
+    model = Income
+    context_object_name = 'income'
+    template_name = 'income_detail.html'
