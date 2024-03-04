4927867d8ee0a143e61667a68fa4a330399cb621
oomat07 <shakiralievomat3@gmail.com>
2024-03-04 17:27:04 +0600

    created forms.py  and editted views

--- apps/Incomes/views.py
+++ apps/Incomes/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import Income
+from django.views.generic.edit import CreateView
+from .forms import IncomeForm
+from django.urls import reverse_lazy
 
 def incomes_list(request):
     incomes = Income.objects.all()
     return render(request, 'incomes_list.html', {'incomes' : incomes})
+
+
+class IncomeCreateView(CreateView):
+    model = Income
+    template_name = 'create_income.html'
+    form_class = IncomeForm
+    success_url = reverse_lazy('incomes_list')
