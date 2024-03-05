69aa4d135a15b1175d6e11dda2bec3909a4211b5
oomat07 <shakiralievomat3@gmail.com>
2024-03-05 15:51:16 +0600

    migrated

--- apps/Expenses/models.py
+++ apps/Expenses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Expense(models.Model):
     category = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_place=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     description = models.TextField(blank=True)
 
