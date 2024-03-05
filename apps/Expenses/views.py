6f11c912a9f9e511df5f78b9b3b1dd18fd7e031c
oomat07 <shakiralievomat3@gmail.com>
2024-03-05 16:26:19 +0600

    created forms and registered in views

--- apps/Expenses/views.py
+++ apps/Expenses/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from .models import Expense
+from .forms import ExpenseForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 
 def expenses_list(request):
     expenses = Expense.objects.all()
     return render(request, 'expenses_list.html', {'expenses' : expenses})
 
+class ExpenseCreateView(CreateView):
+    model = Expense
+    template_name = 'create_expenses.html'
+    form_class = ExpenseForm
+    success_url = reverse_lazy('expenses')
+
+class ExpenseDetailView(DetailView):
+    model = Expense
+    context_object_name = 'expense'
+    template_name = 'expense_detail.html'
--- apps/Incomes/views.py
+++ apps/Incomes/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import Income
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from .forms import IncomeForm
-from django.urls import reverse_lazy
 
 def incomes_list(request):
     incomes = Income.objects.all()
