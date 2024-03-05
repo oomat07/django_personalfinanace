25f8b08ea4d209d4373e7195def43e551f2c63a1
oomat07 <shakiralievomat3@gmail.com>
2024-03-05 15:47:31 +0600

    created model for Expense

--- apps/Expenses/models.py
+++ apps/Expenses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Expense(models.Model):
+    category = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_place=2)
+    date = models.DateField()
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.category} - {self.amount}"
