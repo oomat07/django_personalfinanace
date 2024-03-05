97b27b091812b55c6f71a0cc481b75dd77d8d47e
oomat07 <shakiralievomat3@gmail.com>
2024-03-05 17:44:22 +0600

    created template for analytics and registered

--- PersonalFinance/urls.py
+++ PersonalFinance/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.Incomes.urls')),
-    path('', include('apps.Expenses.urls'))
+    path('', include('apps.Expenses.urls')),
+    path('', include('apps.Analytics.urls'))
 ]
