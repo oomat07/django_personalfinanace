56da272c4a0c5a474ce591b9edf89c9aa4bca05d
oomat07 <shakiralievomat3@gmail.com>
2024-03-05 17:33:59 +0600

    created views for analytics apps

--- apps/Analytics/views.py
+++ apps/Analytics/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from apps.Incomes.models import Income
+from apps.Expenses.models import Expense
+from django.db.models import Sum
 
-# Create your views here.
+def finacial_summary(request):
+    total_income = Income.objects.aggregate(Sum('amount'))['amount__sum'] or 0
+    total_expense = Expense.objects.aggregate(Sum('amount'))['amount__sum'] or 0
+    context = {
+        'total_income' : total_income,
+        'total_expence' : total_expense,
+        'net' : total_income - total_expense
+    }
+    return render(request, 'financial_summary.html', context)
+
